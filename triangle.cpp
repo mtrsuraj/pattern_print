c07e6135ad70577919e868c23091c4f5205e5ac0
mtr_suraj <89896051+surajrepository@users.noreply.github.com>
2022-06-11 17:59:53 +0530

    Add files via upload

---
+++ triangle.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int n=5;
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=5; j++){
+            if(j>=6-i){
+                cout<<"*";
+            }
+            else{
+               cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
